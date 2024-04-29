36a943f7ecdbdbea56d84838e9d02e8adb6517fd
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-29 20:30:52 +0300

    initial commit

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,22 @@
-
+import 
 
 const routes = [
-   
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/actors",
+    element: <Actors />
+  },
+  {
+    path: "/directors",
+    element: <Directors />
+  },
+  {
+    path: "/movies",
+    element: <Movies />
+  }
   ];
 
 export default routes;
